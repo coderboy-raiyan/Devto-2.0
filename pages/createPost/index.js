7982d2351930fa7eb2e749a5aac3c75003fde05b
Raiyan <raiyan5764@gmail.com>
2022-02-09 20:46:45 +0600

    half of create post UI done

--- pages/createPost/index.js
+++ pages/createPost/index.js
@@ -16,8 +16,24 @@ const CreatePost = () => {
         <Header />
       </header>
 
-      <section onClick={() => dispatch(setIsOpen(false))}>
-        <h1 className="p-4 pt-2">Hello</h1>
+      <section
+        className="min-h-screen  bg-gray-100"
+        onClick={() => dispatch(setIsOpen(false))}
+      >
+        {/* editor side */}
+        <div className="grid max-w-3xl grid-cols-1 px-4 md:max-w-6xl md:grid-cols-8 md:px-4 lg:mx-auto lg:max-w-7xl lg:grid-cols-8">
+          <div className="col-span-1 my-4 mx-4 md:col-span-6 lg:col-span-6">
+            {/* editor starts-here */}
+
+            <div className="rounded border bg-white py-6 px-11">
+              <div></div>
+            </div>
+          </div>
+          {/* Editor Suggestions  */}
+          <div className="hidden md:col-span-2 md:inline-grid lg:col-span-2 lg:inline-grid">
+            <h1>Suggestions section</h1>
+          </div>
+        </div>
       </section>
     </>
   );
