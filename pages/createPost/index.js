d3568c528cbb3b5c43983b1e248877e53a53f1f6
Raiyan <raiyan5764@gmail.com>
2022-02-10 21:18:59 +0600

    edit and preview done

--- pages/createPost/index.js
+++ pages/createPost/index.js
@@ -16,7 +16,6 @@ const Editor = dynamic(
 
 const CreatePost = () => {
   const dispatch = useDispatch();
-
   // editor state
   const [editorState, setEditorState] = useState(EditorState.createEmpty());
 
@@ -37,51 +36,56 @@ const CreatePost = () => {
       </header>
 
       <section
-        className="min-h-screen  bg-gray-100"
+        className=" bg-gray-100"
         onClick={() => dispatch(setIsOpen(false))}
       >
-        {/* editor side */}
         <div className="grid max-w-3xl grid-cols-1 px-4 md:max-w-6xl md:grid-cols-8 md:px-4 lg:mx-auto lg:max-w-7xl lg:grid-cols-8">
+          {/* editor side */}
           <div className="col-span-1 my-4 mx-4 md:col-span-6 lg:col-span-6">
-            {/* editor starts-here */}
-
-            <div className="h-full min-h-[500px] rounded border bg-white py-6 px-11">
+            {/* edit and preview button */}
+            <div className="item-center mb-2 flex justify-end space-x-4">
+              <button className="primary-btn font-semibold">Edit</button>
+              <button className="primary-btn">Preview</button>
+            </div>
+            <div className="h-full min-h-[500px] rounded-lg border bg-white py-6">
               <form>
-                {/* cover image */}
-                <div>
-                  <input type="file" className="hidden" />
-                  <button className="rounded-lg border-2 py-3 px-4 text-sm font-semibold text-gray-600">
-                    Add a cover image
-                  </button>
-                </div>
+                <div className="px-11">
+                  {/* cover image */}
+                  <div>
+                    <input type="file" className="hidden" />
+                    <button className="rounded-lg border-2 py-3 px-4 text-sm font-semibold text-gray-600">
+                      Add a cover image
+                    </button>
+                  </div>
 
-                {/* blog title */}
+                  {/* blog title */}
 
-                <div className="my-2">
-                  <input
-                    type="text"
-                    placeholder="New post title here..."
-                    className="w-full border-none text-2xl font-bold placeholder:text-gray-600 focus:ring-0 md:text-3xl lg:text-5xl"
-                  />
-                </div>
+                  <div className="my-2">
+                    <input
+                      type="text"
+                      placeholder="New post title here..."
+                      className="w-full border-none text-2xl font-bold placeholder:tracking-wide placeholder:text-gray-700 focus:ring-0 md:text-3xl lg:text-5xl"
+                    />
+                  </div>
 
-                {/* blog tags */}
-                <div>
-                  <input
-                    type="text"
-                    placeholder="Add upto 4 tags..."
-                    className="w-full border-none placeholder:text-gray-500 focus:ring-0"
-                  />
+                  {/* blog tags */}
+                  <div>
+                    <input
+                      type="text"
+                      placeholder="Add upto 4 tags..."
+                      className="w-full border-none placeholder:tracking-wider placeholder:text-gray-500 focus:ring-0"
+                    />
+                  </div>
                 </div>
 
                 <div className="my-4">
                   <Editor
                     placeholder="Write your post content here..."
-                    className="text-black"
+                    className="text-black "
                     editorState={editorState}
-                    toolbarClassName="!py-3 !border-none !bg-gray-50 !px-5"
+                    toolbarClassName="!py-3 !border-none !bg-[#f9f9f9] !px-11"
                     wrapperClassName="!border-none"
-                    editorClassName="editorClassName"
+                    editorClassName="!px-11"
                     onEditorStateChange={onEditorStateChange}
                     toolbar={{
                       inline: { inDropdown: true },
