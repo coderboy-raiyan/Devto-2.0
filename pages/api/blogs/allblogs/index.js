0b9d33db9b81b32062ea5521bfea7fccb21adaf8
Raiyan <raiyan5764@gmail.com>
2022-03-07 15:55:26 +0600

    pagination done

---
+++ pages/api/blogs/allblogs/index.js
@@ -0,0 +1,31 @@
+import connectDb from "../../../../connectDb/connectDb";
+import allBlogs from "../../../../models/blogSchema";
+connectDb();
+
+export default async function allblogs(req, res) {
+  try {
+    switch (req.method) {
+      case "GET":
+        await getAllBlogs(req, res);
+        break;
+
+      default:
+        await getAllBlogs(req, res);
+        break;
+    }
+  } catch (err) {
+    res.status(500).json({ message: "There is a server side error" });
+  }
+}
+
+// this endpoint is used for load all blogs
+const getAllBlogs = async (req, res) => {
+  try {
+    const count = await allBlogs.countDocuments();
+    const data = await allBlogs.find().sort({ time: -1 });
+    res.send({ blogs: data, size: count });
+    console.log("else", loadCount);
+  } catch (err) {
+    res.status(500).json({ message: "There is a server side error", err });
+  }
+};
