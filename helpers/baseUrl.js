2138e28c84334a0aac3662d4c49be6a50a5a14bb
Raiyan <raiyan5764@gmail.com>
2022-04-09 20:16:34 +0600

    [DONE]-ready for deploy

--- helpers/baseUrl.js
+++ helpers/baseUrl.js
@@ -1,3 +1,6 @@
-const baserUrl = process.env.NODE_ENV === "production" ? "" : "http://localhost:3000/";
+const baserUrl =
+    process.env.NODE_ENV === "production"
+        ? "https://devto-clone-raiyan.vercel.app/"
+        : "http://localhost:3000/";
 
 export default baserUrl;
